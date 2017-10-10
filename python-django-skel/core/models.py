69b9f29d04e6ffafc874953ae59ef53dc4f7941e
felipe.santos <felipe.santos@idealinvest.com.br>
2017-10-10 16:17:46 -0300

    In progress

--- python-django-skel/core/models.py
+++ python-django-skel/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Repository(models.Model):
+    username = models.CharField(max_length=255)
+    repository_id = models.CharField(max_length=511)
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=511)
+    languages = models.CharField(max_length=511)
+    tags = models.CharField(max_length=1023)
--- python-django-skel/core/views.py
+++ python-django-skel/core/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+import os
+
 
 # Create your views here.
+def home(request):
+    user = 'gustavohenrique'
+    query = '''
+      query {
+        user(login: "%s") {
+          starredRepositories(first: 1) {
+            totalCount
+            edges {
+              node {
+                id
+                name
+                url
+                languages(first: 10) {
+                  edges {
+                    node {
+                      name
+                    }
+                  }
+                }
+              }
+              cursor
+            }
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              endCursor
+            }
+          }
+        }
+      }''' % user
+    headers = {'Authorization': 'bearer {0}'.format(os.getenv('GITHUB_API_TOKEN'))}
+    url = 'https://api.github.com/graphql'
+    transport = RequestsHTTPTransport(url, headers=headers, use_json=True)
+    client = Client(transport=transport)
+    resp = client.execute(gql(query))
+    repositories = resp.get('user').get('starredRepositories')
+    print(repositories)
+    return HttpResponse('200')
